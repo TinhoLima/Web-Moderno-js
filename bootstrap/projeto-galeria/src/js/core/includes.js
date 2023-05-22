d5498bac3adf8c00adfe6c80bd302686a103396a
Alison Coutinho <alisonrj.work@gmail.com>
2023-05-21 22:25:22 -0300

    teste header

--- bootstrap/projeto-galeria/src/js/core/includes.js
+++ bootstrap/projeto-galeria/src/js/core/includes.js
@@ -3,13 +3,13 @@ import $ from 'jquery'
 function loadIncludes(parents) {
     // se o parent não for encontrado, busque no corpo todo.
     if (!parent) parent = 'body'
-    $(parent).find('[wm-include]').each(function(i, e) {
-        const url = $(e).attr('wm-include')
+    $(parent).find('[teste]').each(function(i, e) {
+        const url = $(e).attr('teste')
         $.ajax({
             url,
             success(data) {
                 $(e).html(data)
-                $(e).removeAttr('wm-include')
+                $(e).removeAttr('teste')
 
                 loadIncludes(e)
             }
