8ff3c95247194c9c15028bdcf04da565b4300058
Alison Coutinho <93990888+TinhoLima@users.noreply.github.com>
2023-05-26 16:49:00 -0300

    tentativa de correção.

--- bootstrap/projeto-galeria/src/js/core/includes.js
+++ bootstrap/projeto-galeria/src/js/core/includes.js
@@ -1,20 +1,46 @@
-import $ from 'jquery'
+// import $ from 'jquery'
 
-function loadIncludes(parents) {
-    // se o parent não for encontrado, busque no corpo todo.
-    if (!parent) parent = 'body'
-    $(parent).find('[teste]').each(function(i, e) {
-        const url = $(e).attr('teste')
-        $.ajax({
-            url,
-            success(data) {
-                $(e).html(data)
-                $(e).removeAttr('teste')
+// function loadIncludes(parent) {
+//     // se o parent não for encontrado, busque no corpo todo.
+//     if (!parent) parent = 'body'
+//     $(parent).find('[wm-include]').each(function(i, e) {
+//         const url = $(e).attr('wm-include')
+//         $.ajax({
+//             url,
+//             success(data) {
+//                 $(e).html(data)
+//                 $(e).removeAttr('wm-include')
 
-                loadIncludes(e)
-            }
-        })
-    })
+//                 loadIncludes(e)
+//             }
+//         })
+//     })
+// }
+
+// loadIncludes()
+
+import $ from 'jquery';
+
+function loadIncludes(parent) {
+  // Se o parent não for encontrado, busque em todo o corpo.
+  if (!parent) parent = 'body';
+  $(parent).find('[wm-include]').each(function (i, e) {
+    const url = $(e).attr('wm-include');
+    $.ajax({
+      url,
+      success: function (data) {
+        $(e).html(data);
+        $(e).removeAttr('wm-include');
+
+        loadIncludes(e);
+      },
+      error: function (error) {
+        console.log('Ocorreu um erro ao carregar o arquivo:', error);
+      }
+    });
+  });
 }
 
-loadIncludes()
+$(document).ready(function () {
+  loadIncludes();
+});
