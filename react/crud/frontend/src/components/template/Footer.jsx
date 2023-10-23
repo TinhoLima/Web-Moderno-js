c030365663e2abebb5b46e2bffa6b68a3c70b86d
Alison Coutinho <93990888+TinhoLima@users.noreply.github.com>
2023-10-23 10:38:12 -0300

    update

--- react/crud/frontend/src/components/template/Footer.jsx
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        <button type="button" onclick='teste()'>on / off</button>
         <span>
             Desenvolvido com <i className='fa fa-heart text-danger'></i> por<strong> Cod<span className='text-danger'>3</span>r</strong>
         </span>
